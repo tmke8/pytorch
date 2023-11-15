67cc2cf25911af16785442d17e999ff0b4272ad1
soulitzer <soulitzer@gmail.com>
2023-11-15 14:48:42 -0800

    Fix queue_callback error propagation
    ghstack-source-id: e223a9b73182e68b02a230091942bf827fd8098a
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/113702

--- torch/csrc/autograd/python_function.cpp
+++ torch/csrc/autograd/python_function.cpp
@@ -50,22 +50,21 @@ PyObject* THPGradientEdgeClass = nullptr;
 #define THPFunction_assert(condition, ...) \
   if (!(condition)) {                      \
     THPUtils_setError(__VA_ARGS__);        \
-    throw python_error();                  \
+    throw_python_error();                  \
   }
 
 // Anonymous namespace for helpful functions used in this file
 namespace {
 
-// Throw a python_error with the PyErr state persisted, so that we
-// don't lose the error state if the GIL is released when we don't
-// have a PyThreadState created beforehand, this is made so that
-// even for pure C++ thread without a pre-created PyThreadState could
-// also capture the correct error message.
-// TODO: This is a temporary approach to allow C++ thread to correctly
-// capture Python Error in autograd, remove this when c10 thread pool
-// allow to do one time initialization.
-// see discussion in https://github.com/pytorch/pytorch/pull/34845
-// Follow up issue: https://github.com/pytorch/pytorch/issues/35006
+// TODO: I'm not sure why we need to call this function (to persist the
+// error immediately at the callsite) instead of doing `raise python_error()`
+// and relying on the engine to persist for us. But, removing this seems to
+// cause the following test to fail:
+//
+// python test/distributed/rpc/test_tensorpipe_agent.py -k
+// test_backward_autograd_engine_error
+//
+// See Note [ Persisting PyErr state across autograd engine threads ]
 void throw_python_error() {
   python_error err;
   err.persist();
