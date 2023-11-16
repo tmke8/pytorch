99d05a178d7321051cfde77a9dce0b73bd060c2a
chundian <chundian@meta.com>
2023-11-16 00:42:36 -0800

    [inductor] Fix torch.split bug on unbacked symint
    torch.split(x, l) fails when l's shape is the unbacked symint.
    
    E.g. l =
    y.tolist() makes l the unbacked shape, because l depends on the
    data access of y. The downdtream call `SliceView.create()`
    evaluates the shape even if the input shape is unbacked symint,
    which brings up the bug.
    
    Test Plan:
    python test/inductor/test_unbacked_symints.py -k test_split_with_sizes
    
    ghstack-source-id: f5da56ea85537ffac631324a3685977860885189
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/113406

--- torch/_inductor/ir.py
+++ torch/_inductor/ir.py
@@ -2084,11 +2084,8 @@ class SliceView(View):
         start = cls.handle_negative_index(start, new_size[dim])
         end = cls.handle_negative_index(end, new_size[dim])
 
-        end = sizevars.evaluate_min(end, new_size[dim])
-        start = sizevars.evaluate_min(start, end)
-        if start == 0 and sizevars.size_hint(end - new_size[dim]) == 0 and step == 1:
-            sizevars.guard_equals(end, new_size[dim])
-            return x
+        end = sympy.Min(end, new_size[dim])
+        start = sympy.Min(start, end)
 
         new_size[dim] = FloorDiv(end - start + (step - 1), step)
 
